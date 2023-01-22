923097d683b9e5c59e01290cfbdc110930bbe44a
SarojPahi <107534043+SarojPahi@users.noreply.github.com>
2023-01-22 14:56:26 +0530

    server created

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -23,15 +23,12 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch(
-        "https://dalle-arbb.onrender.com/api/v1/post",
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch("http://localhost:8080/api/v1/post", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       if (response.ok) {
         const result = await response.json();
