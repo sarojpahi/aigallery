da9826e6f5712011335052bb2eb3472788efd6d2
SarojPahi <107534043+SarojPahi@users.noreply.github.com>
2023-02-06 16:30:41 +0530

    some change in server

--- server/src/Routes/image.route.js
+++ server/src/Routes/image.route.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { generateImage, variation } = require("../Controller/image.constroller");
+const { postImage, getImage } = require("../Controller/post.controller");
 
 const router = express.Router();
 
 router.post("/", generateImage);
 router.post("/variation", variation);
+router.post("/postImage", postImage);
+router.get("/postImage", getImage);
 
 module.exports = router;
